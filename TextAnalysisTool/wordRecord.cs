1f8e9dcff05293ad11c9a20744f731b81f0fb2c9
Joe Martin <tudno@live.co.uk>
2026-02-11 16:16:44 +0000

    Add files via upload

---
+++ TextAnalysisTool/wordRecord.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace TextAnalysisTool
+{
+    internal class wordRecord
+    {
+        private string word;
+        private int length, occurences;
+        private int line;
+        private List<int> lines;
+
+        public wordRecord(string word, int line)
+        {
+            this.word = word;
+            this.length = word.Length;
+            this.occurences = 1;
+            this.lines = new List<int> { line };
+        }
+
+        public string Word
+        {
+            get { return word; }
+            set { this.word = value; }
+        }
+
+        public int Length
+        {
+            get { return length; }
+            set { this.length = value; }
+        }
+
+        public int Occurences
+        {
+            get { return occurences; }
+            set { this.occurences = value; }
+        }
+
+        public List<int> Lines
+        {
+            get { return lines; }
+            set { lines = value; }
+        }
+    }
+}
